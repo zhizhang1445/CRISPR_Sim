958d57ffa58057b656d981c84fcb1a9180c3bea7
Zhi Zhang <zhi.zhang144515@gmail.com>
2023-08-19 08:44:56 -0700

    please no more

--- Long_Simulation/antigenicWaveSimulation.py
+++ Long_Simulation/antigenicWaveSimulation.py
@@ -23,10 +23,10 @@ def main(params, sim_params):
     params, sim_params = init_cond(params, sim_params)
 
     i = 7
-    foldername = sim_params["foldername"] + f"test{i}"
+    foldername = sim_params["foldername"] + f"/test{i}"
     while os.path.exists(foldername):
         i += 1
-        foldername = sim_params["foldername"] + f"test{i}"
+        foldername = sim_params["foldername"] + f"/test{i}"
 
     try:
         write2json(foldername, params, sim_params)
@@ -80,7 +80,7 @@ if __name__ == '__main__':
         "num_threads":                 32,
         "tail_axis":               [1, 1],
         "t_snapshot":                  10,
-        "foldername":              "Data/",
+        "foldername":            "../Data",
         "seed":                         0,
     }
 
--- Long_Simulation/bashhelper.sh
+++ Long_Simulation/bashhelper.sh
@@ -10,7 +10,5 @@ source ~/.virtualenvs/Env4Zhi/bin/activate
 # Turn off implicit threading in Python, R
 export OMP_NUM_THREADS=40
 
-cd $SCRATCH/Data
-
 python3 antigenicWaveSimulation.py 80
 wait
