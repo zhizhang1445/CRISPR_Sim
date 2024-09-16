97184997094e62a04a0c792a6b85c60901ff5a0f
Zhi Zhang <zhi.zhang144515@gmail.com>
2024-09-16 09:46:35 -0400

    "1D simulation (no immunity)"

--- Scripts/mutation.py
+++ Scripts/mutation.py
@@ -3,12 +3,13 @@ import numpy.ma as ma
 import scipy
 from scipy.ndimage import convolve
 from scipy import signal
+from scipy.sparse import issparse
 from joblib import Parallel, delayed, parallel_backend
 from numpy.random import default_rng
 from concurrent.futures import as_completed
-from supMethods import timeit
+from supMethods import sum_parallel, timeit
 
-def num_mutants_parallel(n, params, sim_params): #TODO PARALLELIZE THIS
+def num_mutants_per_loc(n, params, sim_params): #TODO PARALLELIZE THIS
     mu = params["mu"]
     dt = sim_params["dt"]
 
@@ -37,7 +38,22 @@ def num_mutation(params, sim_params): #Sparse is not needed
         except RecursionError:
             return 0
 
-def mutation_jump(m, params, sim_params):
+def mutation_jump1D(m, params, sim_params):
+    shape_param = params["gamma_shape"]
+    dx = sim_params["dx"]
+
+    jump = 0
+    mean = 2*dx
+    theta = mean/shape_param
+    
+    for i in range(m):
+        angle = np.random.choice([-1, 1]) #Goodammit couldn't this have been clearer??? It's supposed to be isotropic
+        jump = jump + np.random.gamma(shape_param, theta)*angle
+
+    jump = np.round(jump)
+    return jump
+
+def mutation_jump2D(m, params, sim_params):
     shape_param = params["gamma_shape"]
     dx = sim_params["dx"]
 
@@ -53,11 +69,37 @@ def mutation_jump(m, params, sim_params):
     jump = np.round(jump)
     return jump
 
-def mutation(n, params, sim_params):
+def mutation1D(n, params, sim_params):
+    checksum = np.sum(n)
+    mutation_map = num_mutants_per_loc(n, params, sim_params) # The mutation maps tells you how many virus have mutated at each location
+    x_ind = np.nonzero(mutation_map)
+    n_to_add = np.zeros_like(n)
+
+    for x_i in x_ind:
+        num_mutants_at_site = mutation_map[x_ind]
+        n_to_add[x_i] -= num_mutants_at_site
+        for j in range(num_mutants_at_site):
+            num_mutation_at_site = num_mutation(params, sim_params)
+            jump = mutation_jump1D(num_mutation_at_site, params, sim_params)
+
+            try:
+                new_x_loc = (x_i + jump).astype(int)
+                n_to_add[new_x_loc] += 1
+            except IndexError: #Array Out of Bounds
+                if new_x_loc < 0:
+                    new_x_loc = 0
+
+                if new_x_loc >= len(n):
+                    new_x_loc = len(n)-1 #lmao this is gonna be a pain in cpp
+                n_to_add[new_x_loc] += 1
+
+    return n+n_to_add
+
+def mutation2D(n, params, sim_params):
     num_threads = sim_params["num_threads"]
     checksum = np.sum(n)
 
-    mutation_map = num_mutants_parallel(n, params, sim_params) # The mutation maps tells you how many virus have mutated at each location
+    mutation_map = num_mutants_per_loc(n, params, sim_params) # The mutation maps tells you how many virus have mutated at each location
     x_ind, y_ind = np.nonzero(mutation_map) #finding out where the mutations happends
 
     x_ind_subsets = np.array_split(x_ind, num_threads)
@@ -71,7 +113,7 @@ def mutation(n, params, sim_params):
 
             for j in range(num_mutants_at_site): #Find out where those virus have moved to
                 num_mutation_at_site = num_mutation(params, sim_params) #Sampling how many single mutation for a single virus (num of jumps) 
-                jump = mutation_jump(num_mutation_at_site, params, sim_params) #Sampling the jump
+                jump = mutation_jump2D(num_mutation_at_site, params, sim_params) #Sampling the jump
 
                 try:
                     new_x_loc = (x_i + jump[0]).astype(int)
@@ -84,9 +126,9 @@ def mutation(n, params, sim_params):
                         new_y_loc = 0
 
                     if new_x_loc >= n.shape[0]:
-                        new_x_loc = -1 #lmao this is gonna be a pain in cpp
+                        new_x_loc = n.shape[0]-1 #lmao this is gonna be a pain in cpp
                     if new_y_loc >= n.shape[1]:
-                        new_y_loc = -1
+                        new_y_loc = n.shape[1]-1
 
                     n_to_add[new_x_loc, new_y_loc] += 1
         return n_to_add
@@ -95,7 +137,16 @@ def mutation(n, params, sim_params):
     # results = mutation_single(x_ind, y_ind)
     # n = n+results
 
-    n = n+np.sum(results, axis = 0)
+    n = n + sum_parallel(results, axis = 0)
     if checksum != np.sum(n):
         raise ValueError("Cries cuz Bacteria died during mutation")
     return n
+
+def mutation(n, params, sim_params):
+    ndim = sim_params["ndim"]
+    if ndim == 1 and isinstance(n, np.ndarray):
+        return mutation1D(n, params, sim_params)
+    elif ndim == 2 and issparse(n):
+        return mutation2D(n, params, sim_params)
+    else:
+        raise TypeError(f"Something went wrong n_dim: {ndim} but type is {type(n)}")
